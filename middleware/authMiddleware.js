32fc91ca00004aad3b4e8f63b092afbd06d3e89b
Samwuel Simiyu <samwuels254@gmail.com>
2024-12-16 21:38:08 +0300

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,21 +1,20 @@
 const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
-    const token = req.cookies.authToken;  // Get token from cookies
+    const token = req.cookies.authToken;
 
     if (!token) {
         return res.status(401).json({ error: 'No token, authorization denied' });
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);  // Verify the token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded.user;  // Attach the decoded user data to req.user
         next();
     } catch (err) {
-        console.error('Token verification failed:', err);  // Log error if token verification fails
+        console.error('Token verification failed:', err);
         res.status(401).json({ error: 'Token is not valid' });
     }
 };
 
 module.exports = auth;
-
